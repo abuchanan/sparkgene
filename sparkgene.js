c821a45a857f9a0942601865c1cc8525f9ccd524
Alex Buchanan <buchanae@gmail.com>
2013-12-12 11:56:33 -0800

    cleanup and docs

--- sparkgene.js
+++ sparkgene.js
@@ -1,52 +1,74 @@
-var nodeList = document.querySelectorAll('.draw-gene');
+function sparkgene(element) {
 
-for (var i = 0; i < nodeList.length; i++) {
-  var node = nodeList[i];
-
-  var rawPositions = node.dataset.gene.split(',');
-  var positions = [];
-
-  for (var j = 0; j < rawPositions.length; j++) {
-    var rawPos = rawPositions[j];
-    var pos = parseInt(rawPos);
-    positions.push(pos);
-  }
-
-  // TODO check that positions.length is even.
-  //      if it's odd, there's some here
-
-  // TODO better exon connectors
-
-  var default_color = '#00BEF2';
-  var color = node.dataset.geneColor || default_color;
-
-  var default_height = 20;
-  var height = node.dataset.geneHeight || default_height;
-  var width = positions[positions.length - 1] + 10;
-
-  var el = document.createElement('canvas');
-  el.style.width = width + 'px';
-  el.style.height = height + 'px';
-  el.width = width;
-  el.height = height;
-  node.appendChild(el);
-
-  var ctx = el.getContext('2d');
-  ctx.fillStyle = color;
-
-
-  for (var j = 0; j < positions.length - 1; j += 2) {
-    var a = positions[j];
-    var b = positions[j + 1];
-
-    ctx.fillRect(a, 0, b - a + 1, height);
-
-    if (j > 0) {
-      var c = positions[j - 1] + 1;
-      ctx.beginPath();
-      ctx.moveTo(c, height / 2);
-      ctx.lineTo(a, height / 2);
-      ctx.stroke();
+    function isString(obj) {
+      return Object.prototype.toString.call(obj) == '[object String]';
     }
-  }
+
+    var DEFAULT_SELECTOR = '.sparkgene';
+    var DEFAULT_COLOR = '#00BEF2';
+    var DEFAULT_HEIGHT = 20;
+
+    // Handle argument overloading
+    if (element === undefined) {
+      element = DEFAULT_SELECTOR;
+    }
+
+    
+    if (isString(element)) {
+      var nodeList = document.querySelectorAll(element);
+      for (var i = 0, ii = nodeList.length; i < ii; i++) {
+        sparkgene(nodeList[i]);
+      }
+      return;
+    }
+
+    if (!(element instanceof HTMLElement)) {
+      throw 'Error: unrecognized element or selector';
+    }
+
+    // Parse the positions string
+    var rawPositions = element.dataset.gene.split(',');
+    var positions = [];
+
+    for (var j = 0; j < rawPositions.length; j++) {
+      var rawPos = rawPositions[j];
+      var pos = parseInt(rawPos);
+      positions.push(pos);
+    }
+
+    // Figure out some drawing configuration
+    var color = element.dataset.geneColor || DEFAULT_COLOR;
+    var height = element.dataset.geneHeight || DEFAULT_HEIGHT;
+    var width = positions[positions.length - 1] + 10;
+
+    // Create our <canvas> element and append it to the DOM
+    var el = document.createElement('canvas');
+    el.style.width = width + 'px';
+    el.style.height = height + 'px';
+    el.width = width;
+    el.height = height;
+    element.appendChild(el);
+
+    // Draw to the <canvas>
+    var ctx = el.getContext('2d');
+    ctx.fillStyle = color;
+
+    // Loop over the positions with a step of 2
+    for (var j = 0, jj = positions.length - 1; j < jj; j += 2) {
+      var exon_start = positions[j];
+      var exon_end = positions[j + 1];
+
+      var width = exon_end - exon_start + 1;
+      ctx.fillRect(exon_start, 0, width, height);
+
+      // Draw a connector to the previous exon.
+      if (j > 0) {
+        var previous_exon_end = positions[j - 1] + 1;
+        var y_position = height / 2;
+        ctx.beginPath();
+        ctx.moveTo(previous_exon_end, y_position);
+        ctx.lineTo(exon_start, y_position);
+        ctx.stroke();
+      }
+    }
 }
